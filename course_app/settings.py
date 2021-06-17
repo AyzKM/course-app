af2cf0a74c571a95f79d1462a7e433bccc04f316
AyzKM <78482176+AyzKM@users.noreply.github.com>
2021-06-17 15:15:16 +0200

    Updates STATIC_ROOT

--- course_app/settings.py
+++ course_app/settings.py
@@ -29,7 +29,6 @@ DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -129,6 +128,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Default primary key field type
